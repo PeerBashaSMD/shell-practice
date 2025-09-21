048049cc6dd091d0cceb1712ba56a9fad3d63463
PeerBasha <smdpeeru@gmail.com>
2025-09-21 13:05:05 +0530

    some message

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,6 +5,6 @@ PERSON1=John
 PERSON2=Raheem
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
-echo "Suresh:: Hello $PERSON!, I am fine. How are you?"
+echo "$PERSON2:: Hello $PERSON1!, I am fine. How are you?"
 echo "$PERSON1:: What are you learning recently"
-echo "Suresh:: I am learning Shel scripting $PERSON1"
+echo "$PERSON2:: I am learning Shel scripting $PERSON1"
