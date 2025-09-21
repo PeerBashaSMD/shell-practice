4d65f2bfacc705e02d84902775c5f768198873e1
PeerBasha <smdpeeru@gmail.com>
2025-09-21 12:46:45 +0530

    some message

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echi "Suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: What are you learning recently"
+echo "Suresh:: I am learning Shel scripting Rames"
